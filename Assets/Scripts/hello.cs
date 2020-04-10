b080cb8e7f0e742552fc1161226b0a2c36c8106f
WuSiming <wusiming@shiyue.com>
2020-04-10 13:36:22 +0800

    hi

---
+++ Assets/Scripts/hello.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject gameObject = Resources.Load("Block") as GameObject;
+        Debug.Log(gameObject);
+        GameObject block = Instantiate(gameObject, new Vector2(0, 0), gameObject.transform.rotation, GameObject.Find("Canvas/GuildCrushWindow/Bg/Board").transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
