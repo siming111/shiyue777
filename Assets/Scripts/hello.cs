e1807778a145cc6830e731bca0dbee609ca641a8
WuSiming <wusiming@shiyue.com>
2020-04-10 15:11:58 +0800

    1

--- Assets/Scripts/hello.cs
+++ Assets/Scripts/hello.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hello : MonoBehaviour {
 
 
 	// Use this for initialization
 	void Start () {
-        float space = 150f;
-        float shiftX = 0f;
-        float shiftY = 0f;
+        float space = 140f;
+        float shiftX = 87f;
+        float shiftY = -142f;
         GameObject gameObject = Resources.Load("Block") as GameObject;
         Debug.Log(gameObject);
-        for (int i = 1; i <= 9; i++)
+        for (int i = 0; i < 9; i++)
         {
-            for (int j = 1; j <= 7; j++)
+            for (int j = 0; j < 7; j++)
             {
                 GameObject block = Instantiate(gameObject, GameObject.Find("Canvas/GuildCrushWindow/Bg/Board").transform);
                 block.transform.localPosition = new Vector3(shiftX+i*space, shiftY-j*space, 0);
+                Image image = block.GetComponent<Image>();
+                Debug.Log(image);
+                image.sprite = Resources.Load("Textures/Crush/icon2") as Sprite;
             }
         }
     }
