2bc8b20852f5161a9ebc0e1e6c6dd2a674f655c4
WuSiming <wusiming@shiyue.com>
2020-04-10 14:10:02 +0800

    1

--- Assets/Scripts/hello.cs
+++ Assets/Scripts/hello.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class hello : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
+        float space = 150f;
+        float shiftX = 0f;
+        float shiftY = 0f;
         GameObject gameObject = Resources.Load("Block") as GameObject;
         Debug.Log(gameObject);
-        GameObject block = Instantiate(gameObject, new Vector2(0, 0), gameObject.transform.rotation, GameObject.Find("Canvas/GuildCrushWindow/Bg/Board").transform);
-	}
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 7; j++)
+            {
+                GameObject block = Instantiate(gameObject, GameObject.Find("Canvas/GuildCrushWindow/Bg/Board").transform);
+                block.transform.localPosition = new Vector3(shiftX+i*space, shiftY-j*space, 0);
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void InitLocation()
+    {
+
+    }
 }
