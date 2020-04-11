55800b3fe4d7dd3922c8ed7b742e96f664175103
WuSiming <wusiming@shiyue.com>
2020-04-11 11:59:39 +0800

    1

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        //Button button = GetComponent<Button>();
+        //button.onClick.AddListener(Hello);
 	}
 	
 	// Update is called once per frame
