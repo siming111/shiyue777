04553b167a8e0b0ccf52a2c4b54f88af4ea6e97f
WuSiming <wusiming@shiyue.com>
2020-04-16 09:42:12 +0800

    123

---
+++ Assets/Scripts/TestPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            this.gameObject.SetActive(true);
+        }
+	}
+
+    public void Confirm()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
